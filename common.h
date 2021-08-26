37827da950e0faa9199353637732cd3990ff91c7
Frityet <ambhogal01@gmail.com>
2021-08-25 17:09:26 -0700

    Added metadata

---
+++ common.h
@@ -0,0 +1,17 @@
+//
+// Created by frityet on 2021-08-25.
+//
+
+#ifndef HOI4_MOD_TOOL_COMMON
+#define HOI4_MOD_TOOL_COMMON
+
+#include <string>
+
+using std::string;
+
+struct {
+    const string NAME = "HOI4 Mod Tool";
+    const string VERSION = "1.0.0";
+} program_info;
+
+#endif //HOI4_MOD_TOOL_COMMON
--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
+#include "common.h"
+
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << program_info.NAME << " v" << program_info.VERSION << std::endl;
     return 0;
 }
