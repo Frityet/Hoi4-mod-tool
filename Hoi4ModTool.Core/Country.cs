a09fcf3d51d2a1493666e553ebcb70b44100d11e
KimWithSomething <87281392+KimWithSomething@users.noreply.github.com>
2021-08-28 15:45:37 -0400

    added country.cs

---
+++ Hoi4ModTool.Core/Country.cs
@@ -0,0 +1,15 @@
+namespace Hoi4ModTool.Core
+{
+    public class Country
+    {
+        public string CountryName { get; set; }
+
+        public string CountryTag { get; set; }
+        
+        public string CountryRulingParty { get; set; }
+
+        public string CountryCulture { get; set; }
+
+        public string CountryColor { get; set; }
+    }
+}
